2d83f9ee3107b1fcc491a6c42c57636cd8c0da9d
sargav <s0533130211@gmail.com>
2026-09-23 01:17:01 +0300

    Add automatic image compression

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/Sroia/',
   plugins: [
     react(),
     tailwindcss(),
+    ViteImageOptimizer(),
   ],
 })
