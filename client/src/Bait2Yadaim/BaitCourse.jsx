143dfcfc5c5a2ea4fb0e9853fd11069328005717
sara.code() <s0533130211@gmail.com>
2026-09-23 23:36:47 +0300

    Change GREEN color hex value in BaitCourse component

--- client/src/Bait2Yadaim/BaitCourse.jsx
+++ client/src/Bait2Yadaim/BaitCourse.jsx
@@ -34,7 +34,7 @@ import RecommendationsSection from '../components/RecommendationsSection';
 import TestimonialVideos from '../components/TestimonialVideos';
 
 const BaitCourse = () => {
-    const GREEN = '#7CB343';
+    const GREEN = '#7CB344';
     const GREEN_DARK = '#5a8a2c';
     const DARK = '#1f1f1f';
     const CREAM = '#faf7ec';
